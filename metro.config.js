0969bc08ad11386eb47141d3cc4eadbcd5cb9ffd
Northern Obsidian <obsidian.north.corp@gmail.com>
2026-07-28 14:37:40 +0300

    new release III

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,20 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push('sql');
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.startsWith('@/assets/')) {
+    const filePath = moduleName.replace('@/assets/', '');
+    return context.resolveRequest(
+      context,
+      path.resolve(__dirname, 'assets', filePath),
+      platform,
+    );
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
